842b006084ccd10a34a507e068a0cc03319250c0
Caroline Peri <caroline.anne.peri@gmail.com>
2023-07-20 14:04:21 -0400

    update seed file to seed db with pastas from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+CSV.foreach(Rails.root.join('lib/data/pasta.csv'), headers: true) do |row|
+  Pasta.create({name: row[0], description: row[1]})
+end
