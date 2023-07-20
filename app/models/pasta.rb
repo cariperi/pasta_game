637af9d15b1e1a18e51fb234d24e3976a92be1f1
Caroline Peri <caroline.anne.peri@gmail.com>
2023-07-20 15:43:17 -0400

    add methods to pasta model

--- app/models/pasta.rb
+++ app/models/pasta.rb
@@ -1,3 +1,11 @@
 class Pasta < ApplicationRecord
+  def self.pick_two_names(right_answer)
+    bank = pluck(:name)
+    bank.delete(right_answer)
+    bank.sample(2)
+  end
 
+  def self.get_a_pasta
+    all.sample
+  end
 end
